0e47db04ed74b6a1972708a655575f740ad03083
NeyderSmith <nstolosa@ucundinamarca.edu.co>
2021-09-13 22:55:11 -0500

    '''

---
+++ src/actividad/Actividad.java
@@ -0,0 +1,13 @@
+
+package actividad;
+
+public class Actividad {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("vamos equipo");
+       
+    }
+    
+}
